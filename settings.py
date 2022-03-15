d1ab28c4fee64a51d5bb12a084495e74a257864a
Vusaldidaaaaa <99710983+Vusaldidaaaaa@users.noreply.github.com>
2022-03-15 17:17:12 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-TOKEN = "1619631738:AAHF6r_sQJEWavRqN-sPK4m-YDSIaMYlbAM"
+TOKEN = "5200257640:AAHbQptMwNMEF22cXL-aA0bJJHg2cz7FsyY"
 with open('reqem.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
